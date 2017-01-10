80ee59385a73bbcdf1bc2d66361986aa5ad3f2c5
Gao Wang <gaow@uchicago.edu>
2017-01-09 21:15:08 -0600

    update

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,4 +1,4 @@
-var documentationDict={"Syntax-1":"syntax","Design-and-Features-1":"design"}
+var documentationDict={"Command-interface-1":"interface","Implementation-details-1":"implementation","Syntax-1":"syntax","Design-and-Features-1":"design"}
 var tutorialsDict=}
 var images=]
 var documentation]
