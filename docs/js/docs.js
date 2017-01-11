b75ee2a7d70e1492c20468776e21de4c0140bdf3
Gao Wang <gaow@uchicago.edu>
2017-01-11 14:51:08 -0600

    update

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,5 +1,5 @@
 var documentationDict={"Syntax-1":"Syntax","Command-interface-1":"Command_Options","Design-and-Features-1":"Design_and_Features","Implementation-details-1":"Implementation_Details"}
-var tutorialsDict={"Quick-start-tutorial-1":"Quick_Start","Multiple-DSC-sequences-1":"Multiple_Sequences","Working-in-Python-1":"Mixed_Languages","Update-existing-DSC-1":"Expand_Existing_DSC","Debugging-in-DSC2-1":"Debug_Tips","An-R-example:-`ashr`-benchmark-1":"Intermediate_R_1"}
+var tutorialsDict={"Explore-DSC-output-1":"Explore_Output","Quick-start-tutorial-1":"Quick_Start","Multiple-DSC-sequences-1":"Multiple_Sequences","Working-in-Python-1":"Mixed_Languages","Update-existing-DSC-1":"Expand_Existing_DSC","Debugging-in-DSC2-1":"Debug_Tips","An-R-example:-`ashr`-benchmark-1":"Intermediate_R_1"}
 var images=[]
 var documentation=["Design_and_Features", "Syntax", "Command_Options", "Implementation_Details"]
-var tutorials=["Quick_Start", "Expand_Existing_DSC", "Multiple_Sequences", "Mixed_Languages", "Intermediate_R_1", "Debug_Tips"]
+var tutorials=["Quick_Start", "Explore_Output", "Expand_Existing_DSC", "Multiple_Sequences", "Mixed_Languages", "Intermediate_R_1", "Debug_Tips"]
