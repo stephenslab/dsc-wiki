0bba52efb515f532a5edac07efa5d55230079557
Gao Wang <gaow@uchicago.edu>
2017-01-12 11:11:44 -0600

    update

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,5 +1,5 @@
-var documentationDict={"Syntax-1":"Syntax","Command-interface-1":"Command_Options","Design-and-Features-1":"Design_and_Features","Implementation-details-1":"Implementation_Details"}
-var tutorialsDict={"Explore-DSC-output-1":"Explore_Output","Quick-start-tutorial-1":"Quick_Start","Multiple-DSC-sequences-1":"Multiple_Sequences","Working-in-Python-1":"Mixed_Languages","Update-existing-DSC-1":"Expand_Existing_DSC","Debugging-in-DSC2-1":"Debug_Tips","An-R-example:-ashr-benchmark-1":"Intermediate_R_1"}
-var images=["cmd.png","fancybox_sprite.png","setup.png","course.png","design.png"]
-var documentation=["Design_and_Features", "Syntax", "Command_Options", "Implementation_Details"]
+var documentationDict={"Implementation-details-1":"Implementation_Details","Syntax:-DSC-annotation-1":"DCS_Annotation","Syntax:-DSC-configuration-1":"DSC_Configuration","Command-interface-1":"Command_Options","Syntax:-DSC-execution-1":"DSC_Execution","Terminology-1":"Terminology","Design-and-Features-1":"Design_and_Features"}
+var tutorialsDict={"Quick-start-tutorial-1":"Quick_Start","Working-in-Python-1":"Mixed_Languages","Multiple-DSC-sequences-1":"Multiple_Sequences","An-R-example:-ashr-benchmark-1":"Intermediate_R_1","Debugging-in-DSC2-1":"Debug_Tips","Update-existing-DSC-1":"Expand_Existing_DSC","Explore-DSC-output-1":"Explore_Output"}
+var images=["cmd.png","setup.png","design.png","fancybox_sprite.png","course.png"]
+var documentation=["Terminology", "Design_and_Features", "DSC_Configuration", "DSC_Execution", "DSC_Annotation", "Command_Options", "Implementation_Details"]
 var tutorials=["Quick_Start", "Explore_Output", "Expand_Existing_DSC", "Multiple_Sequences", "Mixed_Languages", "Debug_Tips", "Intermediate_R_1"]
