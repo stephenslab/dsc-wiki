3f5f149fd8973ef497b9470fa5bbd9c954199130
Gao Wang <gaow@uchicago.edu>
2017-07-11 17:35:44 -0500

    Build website with jnbinder.

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,6 +1,6 @@
-var manualArray = []
+var manualArray = ["Terminology", "Design_and_Features", "DSC_Configuration", "DSC_Execution", "DSC_Annotation", "Command_Options", "Implementation_Details"]
 var manualDict = {"Command-interface-1":"Command_Options","Syntax:-DSC-annotation-1":"DSC_Annotation","Syntax:-DSC-configuration-1":"DSC_Configuration","Syntax:-DSC-execution-1":"DSC_Execution","Design-and-Features-1":"Design_and_Features","Implementation-details-1":"Implementation_Details","Terminology-1":"Terminology","Download-and-Installation-1":"installation"}
-var tutorialsArray = []
+var tutorialsArray = ["Quick_Start", "Explore_Output", "Expand_Existing_DSC", "Multiple_Sequences", "Mixed_Languages", "Debug_Tips", "Intermediate_R_1", "Intermediate_R_2"]
 var tutorialsDict = {"Debugging-in-DSC2-1":"Debug_Tips","Update-existing-DSC-1":"Expand_Existing_DSC","Explore-DSC-output-1":"Explore_Output","An-R-example:-ashr-benchmark-1":"Intermediate_R_1","Same-example,-different-style:-ashr-benchmark-1":"Intermediate_R_2","Working-in-Python-1":"Mixed_Languages","Multiple-DSC-sequences-1":"Multiple_Sequences","Quick-start-tutorial-1":"Quick_Start"}
 var examplesArray = []
 var examplesDict = {}
