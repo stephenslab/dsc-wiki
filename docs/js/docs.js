cf2666c1b7fa90522d04e17383fe335c7105b655
Gao Wang <gaow@uchicago.edu>
2018-02-18 09:19:32 -0600

    Build website with jnbinder.

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,8 +1,8 @@
-var manualArray = ['Design_and_Features', 'Terminology', 'DSC_Configuration', 'DSC_Execution', 'DSC_Filtering', 'Command_Options', 'Implementation_Details', 'Road_Map']
+var manualArray = []
 var manualDict = {"Command-interface-1":"Command_Options","DSC-syntax:-basics-of-modules-1":"DSC_Configuration","DSC-syntax:-basics-of-benchmark-execution-1":"DSC_Execution","DSC-Filtering-Syntax-1":"DSC_Filtering","Design-and-Features-1":"Design_and_Features","Implementation-details-1":"Implementation_Details","New-DSC-syntax-and-query-demo-1":"New_Syntax_and_Query","Release-1.0-roadmap-1":"Road_Map","Terminology-1":"Terminology","Download-and-Installation-1":"installation"}
-var tutorialsArray = ['Quick_Start', 'Explore_Output', 'Expand_Existing_DSC', 'Multiple_Pipelines', 'Debug_Tips', 'Intermediate_R_1']
+var tutorialsArray = []
 var tutorialsDict = {"Debugging-in-DSC2-1":"Debug_Tips","Update-existing-DSC-1":"Expand_Existing_DSC","Explore-DSC-output-1":"Explore_Output","An-R-example:-ashr-benchmark-1":"Intermediate_R_1","Same-example,-different-style:-ashr-benchmark-1":"Intermediate_R_2","Working-in-Python-1":"Mixed_Languages","Multiple-DSC-pipelines-1":"Multiple_Pipelines","Quick-start-tutorial-1":"Quick_Start"}
 var examplesArray = []
 var examplesDict = {}
-var playgroundArray = ['Overview', 'Q1', 'Q2']
+var playgroundArray = []
 var playgroundDict = {"DSC-summary-&-query-1":"Overview","DSC-summary-&-query-1":"Q1","DSC-summary-&-query-1":"Q2"}
